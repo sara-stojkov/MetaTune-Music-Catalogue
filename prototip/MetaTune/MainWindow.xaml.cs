48edfe10319632c23de975b2b76d98a296ab07d5
MihajloMilojevic <milojevicm374@gmail.com>
2025-10-28 17:26:35 +0100

    background sound

--- prototip/MetaTune/MainWindow.xaml.cs
+++ prototip/MetaTune/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace MetaTune
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer _player = new MediaPlayer();
+        private bool paused = false;
         public MainWindow()
         {
             InitializeComponent();
+            PlayBackgroundMusic();
+        }
+        private void PlayBackgroundMusic()
+        {
+            var uri = new Uri("assets/ambient.mp3", UriKind.Relative);
+            _player.Open(uri);
+            _player.MediaEnded += (s, e) =>
+            {
+                _player.Position = TimeSpan.Zero; // Loop
+                _player.Play();
+            };
+            _player.MediaOpened += (s, e) =>
+            {
+                _player.Play();
+                paused = false;
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (paused)
+            {
+                _player.Play();
+                paused = false;
+                playPause.Content = "Pause";
+            }
+            else
+            {
+                _player.Pause();
+                paused = true;
+                playPause.Content = "Play";
+            }
         }
     }
 }
