28d13c36c8dd81c4da72a5b6da2affb413ddb47d
vasilicbosko <vasilic.bosko@jjzmaj.edu.rs>
2025-10-29 22:12:28 +0100

    Feat: editor crud functions

--- prototip/MetaTune/View/Admin/AddEditorDialog.xaml.cs
+++ prototip/MetaTune/View/Admin/AddEditorDialog.xaml.cs
@@ -1,22 +1,39 @@
-﻿
-
+﻿using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using MetaTune.ViewModel.Admin;
+using Core.Model;
 
 namespace MetaTune.View.Admin
 {
     public partial class AddEditorDialog : Window
     {
-        public AddEditorDialog()
+        public AddEditorDialog() : this(false, null)
         {
-            InitializeComponent();
         }
 
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        public AddEditorDialog(bool isEdit, Core.Model.User existingUser)
         {
-            // Get selected genres
-            var selectedGenres = GenreListBox.SelectedItems;
-            DialogResult = true;
-            Close();
+            InitializeComponent();
+
+
+            DataContext = new AddEditorDialogViewModel(isEdit, existingUser);
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is AddEditorDialogViewModel vm)
+                vm.Password = ((PasswordBox)sender).Password;
+        }
+
+        private void GenreListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataContext is AddEditorDialogViewModel vm)
+            {
+                vm.SelectedGenres.Clear();
+                foreach (Genre g in ((ListBox)sender).SelectedItems.Cast<Genre>())
+                    vm.SelectedGenres.Add(g);
+            }
         }
     }
 }
